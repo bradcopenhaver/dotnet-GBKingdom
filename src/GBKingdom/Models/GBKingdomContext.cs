10b75ebe001a7b39d3d2f48ee609df7212776ebe
bradcopenhaver <bradcopenhaver@gmail.com>
2017-02-10 08:57:01 -0800

    create project file structure and add dependencies for MVC and database migration, set up shared layout and HomeController and Index view

---
+++ src/GBKingdom/Models/GBKingdomContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace GBKingdom.Models
+{
+    public class GBKingdomContext : DbContext
+    {
+        
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=GBKingdom;integrated security=True");
+        }
+    }
+}
