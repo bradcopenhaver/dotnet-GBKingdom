2574dec9e9c1949eab11dd842022535066319e83
bradcopenhaver <bradcopenhaver@gmail.com>
2017-02-10 12:59:28 -0800

    add create review view and function

--- src/GBKingdom/Controllers/ReviewsController.cs
+++ src/GBKingdom/Controllers/ReviewsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using GBKingdom.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,19 @@ namespace GBKingdom.Controllers
 {
     public class ReviewsController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private GBKingdomContext db = new GBKingdomContext();
+        public IActionResult Create(int id)
         {
+            Product thisProduct = db.Products.FirstOrDefault(prod => prod.ProductId == id);
+            ViewBag.product = thisProduct;
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Review review)
+        {
+            db.Reviews.Add(review);
+            db.SaveChanges();
+            return RedirectToAction("Details", "Products", new { id = review.ProductId });
         }
     }
 }
