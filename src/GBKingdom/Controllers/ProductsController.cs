51b905ec6222b3c5478a3e714ecbff52e01071bb
bradcopenhaver <bradcopenhaver@gmail.com>
2017-02-10 12:25:55 -0800

    add product details view plus delete and edit views and functions for products

--- src/GBKingdom/Controllers/ProductsController.cs
+++ src/GBKingdom/Controllers/ProductsController.cs
@@ -28,5 +28,39 @@ namespace GBKingdom.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public IActionResult Details(int id)
+        {
+            Product thisProduct = db.Products
+                .Include(prod => prod.Reviews)
+                .FirstOrDefault(products => products.ProductId == id);
+            return View(thisProduct);
+        }
+        public IActionResult Edit(int id)
+        {
+            Product thisProduct = db.Products.FirstOrDefault(prod => prod.ProductId == id);
+            return View(thisProduct);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            db.Entry(product).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Details", new { id = product.ProductId });
+        }
+        public IActionResult Delete(int id)
+        {
+            Product thisProduct = db.Products
+                .Include(prod => prod.Reviews)
+                .FirstOrDefault(prod => prod.ProductId == id);
+            return View(thisProduct);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Product thisProduct = db.Products.FirstOrDefault(prod => prod.ProductId == id);
+            db.Products.Remove(thisProduct);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
