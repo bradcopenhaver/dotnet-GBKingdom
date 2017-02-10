fe876ff1dcf80f4784fa332fa260e5a5689b3ed6
bradcopenhaver <bradcopenhaver@gmail.com>
2017-02-10 14:18:35 -0800

    add edit function and view for reviews

--- src/GBKingdom/Controllers/ReviewsController.cs
+++ src/GBKingdom/Controllers/ReviewsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using GBKingdom.Models;
+using Microsoft.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -24,6 +25,34 @@ namespace GBKingdom.Controllers
             db.Reviews.Add(review);
             db.SaveChanges();
             return RedirectToAction("Details", "Products", new { id = review.ProductId });
+        }
+        public IActionResult Edit(int id)
+        {
+            Review thisReview = db.Reviews.FirstOrDefault(rev => rev.ReviewId == id);
+            return View(thisReview);
+        }
+        [HttpPost]
+        public IActionResult Edit(Review review)
+        {
+            db.Entry(review).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Details", "Products", new { id = review.ProductId });
+        }
+        public IActionResult Delete(int id)
+        {
+            Review thisReview = db.Reviews
+                .Include(rev => rev.Product)
+                .FirstOrDefault(rev => rev.ReviewId == id);
+            ViewBag.productName = thisReview.Product.Name;
+            return View(thisReview);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            Review thisReview = db.Reviews.FirstOrDefault(rev => rev.ReviewId == id);
+            db.Reviews.Remove(thisReview);
+            db.SaveChanges();
+            return RedirectToAction("Details", "Products", new { id = thisReview.ProductId });
         }
     }
 }
